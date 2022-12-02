356a7ffa33f18daece7f20250f766dcf52be716a
w3arthur <w3arthur@gmail.com>
2022-12-02 07:05:17 +0200

    refactoring

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const api = require('./api');
 const { getLocalFile } = api.localFiles;
 const { app, express, server } = api.express;
 const routers = require('./routers');
-const { rtmpAuthRouter, arduinoSerialRouter, arduinoIOTRouter, commentsRouter } = routers;
+const { /*rtmpAuthRouter,*/ arduinoSerialRouter, arduinoIOTRouter, commentsRouter } = routers;
 const { wsServer1 } = require('./sockets');
 
 //middleware:
@@ -16,19 +16,15 @@ app.use(express.urlencoded({ extended: false }));
 //http:
 app.use("/", express.static(getLocalFile()));  //global public folder
 app.route("/").get(async (req, res) => res.status(200).sendFile(getLocalFile("index.html")));
-app.route("/test").get(async (req, res) => {
-    //await CommentModel( {message : 'test2'} ).save();
-    //await DisplayTextModel( {message : 'test2'} ).save();
-    return res.status(200).sendFile(getLocalFile("test.html"))
-});
-app.use("/api/rtmp_auth", rtmpAuthRouter);
-app.use("/api/arduinoSerial", arduinoSerialRouter);
+//app.route("/test").get(async (req, res) => { return res.status(200).sendFile(getLocalFile("test.html")) }); // not required
+//app.use("/api/rtmp_auth", rtmpAuthRouter);    // using youtube instead nginx streaming
+app.use("/api/arduinoSerial", arduinoSerialRouter); // not in use for now. + wsServer1 not needed
 app.use("/api/arduinoIOT", arduinoIOTRouter);
 app.use("/api/comments", commentsRouter);
 
-app.route("*").all((req, res) => res.status(404).send("fail " + req.path));
+app.route("*").all((req, res) => res.status(404).send("no fit request, fail " + req.path));
 
 //websocket:
-wsServer1(server, app, '/ws_arduino');  //ws explain examples https://stackoverflow.com/questions/22429744/how-to-setup-route-for-websocket-server-in-express
+wsServer1(server, app, '/ws_arduino'); // not in use, used only for serial port  //ws explain examples https://stackoverflow.com/questions/22429744/how-to-setup-route-for-websocket-server-in-express
 
 server.listen(PORT, () => console.log(`${(new Date().toISOString())} Server is listening on port ${PORT}, (Express + WS)`));
